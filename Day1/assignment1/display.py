72844c48ce59e5dc5b742ce1eb4c2bbe19e79832
p142325 <p142325@pythonskillup.com>
2018-03-21 07:03:24 -0700

    Pythonskillup Day1->assignment1

---
+++ Day1/assignment1/display.py
@@ -0,0 +1,8 @@
+'''
+print welcome message
+Created on 21 Mar 2018
+
+@author: p142325
+'''
+def greet():
+    print("Welcome to Python")
---
+++ Day1/assignment1/first.py
@@ -0,0 +1,13 @@
+'''
+Display message using greet funcion.
+Created on 21 Mar 2018
+
+@author: p142325
+'''
+from assignment1.display import greet
+
+def main():
+    greet()
+    
+main()
+    
